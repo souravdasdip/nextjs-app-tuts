3b1dc8c9d2462c82cb9aeba04907904b168fb9b7
souravdasdip <souravdasdip65@gmail.com>
2022-06-29 22:28:40 +0600

    Created Link Routing

--- pages/blogs/[blogid].js
+++ pages/blogs/[blogid].js
@@ -1,12 +1,23 @@
 import { useRouter } from 'next/router';
+import Layout from '../../componets/Layout';
 
 const Blog = () => {
   const router = useRouter();
   const { blogid } = router.query;
   return (
-    <>
+    <Layout>
       <h1>Blog - {blogid}</h1>
-    </>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem nulla
+        repudiandae sint facilis, sunt corrupti numquam id illo. Ut deserunt
+        animi iste voluptatum! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Repellendus maxime rem earum repudiandae, cum possimus
+        quae assumenda nulla culpa. Odit architecto repellendus non, unde
+        recusandae placeat nisi perferendis quod nesciunt! Dolorum sapiente et
+        sint consequuntur earum blanditiis iusto reprehenderit molestiae quia
+        eligendi? Exercitationem, officia nobis!
+      </div>
+    </Layout>
   );
 };
 
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,9 +1,26 @@
 import Layout from '../../componets/Layout';
+import Link from 'next/link';
+import styles from '../../styles/Blogs.module.css';
 
 const Blogs = () => {
   return (
     <Layout>
       <h1>Blogs</h1>
+      <Link href={'/blogs/learn-javascript'}>
+        <div className={styles.blogItem}>
+          <h3>How to learn JavaScript in 2022?</h3>
+        </div>
+      </Link>
+      <Link href={'/blogs/learn-react'}>
+        <div className={styles.blogItem}>
+          <h3>How to learn React in 2022?</h3>
+        </div>
+      </Link>
+      <Link href={'/blogs/learn-next'}>
+        <div className={styles.blogItem}>
+          <h3>How to learn Next in 2022?</h3>
+        </div>
+      </Link>
     </Layout>
   );
 };
